d9ef2348cce7f0343afbc820196feebd273c291b
Eli Fatsi <eli.fatsi@viget.com>
2017-05-25 16:36:51 -0600

    Enable toggling of actions

--- src/backend/history.js
+++ src/backend/history.js
@@ -1,11 +1,23 @@
 export function trackHistory(hook, bridge) {
   const repo = hook.repo
+  const history = repo.history
 
-  repo.history.on('reconcile', function() {
-    bridge.send('history:reconcile', JSON.stringify(repo.history))
+  history.on('append', action => {
+    bridge.on(`toggle:${action.id}`, () => history.toggle(action))
   })
 
-  repo.history.on('release', function() {
+  history.on('remove', action => {
+    bridge.removeAllListeners(`toggle:${action.id}`)
+  })
+
+  history.on('reconcile', function() {
+    bridge.send('history:reconcile', JSON.stringify(history))
+  })
+
+  history.on('release', function() {
     bridge.send('history:release', JSON.stringify(repo.state))
   })
+
+  // Force a reconciliation
+  history.checkout()
 }
