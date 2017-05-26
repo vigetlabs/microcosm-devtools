bf9738a80cb9704f0accbdf205d0d87c57978ee8
Eli Fatsi <eli.fatsi@viget.com>
2017-05-26 17:23:34 -0600

    Send history:reconcile with updated history on every toggle/remove
    - Useful if toggling/removing inactive or disabled actions.
      History doesn't reconcile in those cases, but we still
      want to work with an updated History tree.
      Does cause a double history:reconcile bridge event if
      toggling/removing an active action ... can fix that later.

--- src/backend/history.js
+++ src/backend/history.js
@@ -3,15 +3,25 @@ export function trackHistory(hook, bridge) {
   const history = repo.history
 
   history.on('append', action => {
-    bridge.on(`toggle:${action.id}`, () => history.toggle(action))
-    bridge.on(`remove:${action.id}`, () => history.remove(action))
+    bridge.on(`toggle:${action.id}`, () => {
+      history.toggle(action)
+      // if toggling an inactive action, history won't reconcile
+      // we still want to know what history looks like though
+      bridge.send('history:reconcile', JSON.stringify(history))
+    })
+
+    bridge.on(`remove:${action.id}`, () => {
+      history.remove(action)
+      bridge.send('history:reconcile', JSON.stringify(history))
+    })
+
     bridge.on(`checkout:${action.id}`, () => history.checkout(action))
   })
 
   history.on('remove', action => {
     bridge.removeAllListeners(`toggle:${action.id}`)
     bridge.removeAllListeners(`remove:${action.id}`)
-    bridge.removeAllListeners(`checkoute:${action.id}`)
+    bridge.removeAllListeners(`checkout:${action.id}`)
   })
 
   history.on('reconcile', function() {
