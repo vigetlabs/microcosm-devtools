0d9bb75dd72be8f4e74bc336a66228dabc3f10fc
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-05-26 14:15:21 -0400

    Add right sidebar

--- src/devtools/views/header/index.js
+++ src/devtools/views/header/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ToggleLeftRail from './toggle-left-rail'
+import ToggleRightRail from './toggle-right-rail'
 import css from './header.css'
 
 class Header extends React.Component {
+  static defaultProps = {
+    rightRail: true,
+    leftRail: true
+  }
+
   render() {
-    const { status, leftRail, onLeftRail } = this.props
+    const { status, leftRail, onLeftRail, rightRail, onRightRail } = this.props
 
     return (
       <header className={css.header}>
@@ -12,6 +18,7 @@ class Header extends React.Component {
           {status}
         </span>
         <ToggleLeftRail active={leftRail} onClick={onLeftRail} />
+        <ToggleRightRail active={rightRail} onClick={onRightRail} />
       </header>
     )
   }
