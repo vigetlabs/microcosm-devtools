9fce512d3c40b7755bf688f196db73dc10af0403
Prayash Thapa <prayasht@gmail.com>
2017-08-01 17:55:22 -0600

    Add resizing functionality to left and right rails

--- src/devtools/views/right-rail/index.js
+++ src/devtools/views/right-rail/index.js
@@ -4,9 +4,16 @@ import ActionFocus from './action-focus'
 import DataView from './data-view'
 import css from './right-rail.css'
 
+let startX
+let startWidth
+
 class RightRail extends Presenter {
   static defaultProps = {
     open: true
+  }
+
+  state = {
+    paneWidth: 0.25 * window.innerWidth
   }
 
   getModel() {
@@ -17,16 +24,46 @@ class RightRail extends Presenter {
     }
   }
 
+  initDrag = e => {
+    startX = e.clientX
+    startWidth = this.state.paneWidth
+
+    document.documentElement.addEventListener('mousemove', this.onDrag, false)
+    document.documentElement.addEventListener('mouseup', this.stopDrag, false)
+  }
+
+  onDrag = e => {
+    this.setState({
+      paneWidth: startWidth - (e.clientX - startX)
+    })
+  }
+
+  stopDrag = e => {
+    document.documentElement.removeEventListener(
+      'mousemove',
+      this.onDrag,
+      false
+    )
+    document.documentElement.removeEventListener(
+      'mouseup',
+      this.stopDrag,
+      false
+    )
+  }
+
   render() {
     const { open } = this.props
     const { snapshot, focused } = this.model
+    const { paneWidth } = this.state
 
     if (!open) {
       return null
     }
 
     return (
-      <aside className={css.container}>
+      <aside className={css.container} style={{ width: paneWidth + 'px' }}>
+        <div className={css.resizer} onMouseDown={this.initDrag} />
+
         <section>
           <header className={css.header}>
             <h2 className={css.title}>State</h2>
