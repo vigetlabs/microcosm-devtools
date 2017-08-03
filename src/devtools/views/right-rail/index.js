18f78951a8cc8aacb77fafcccd66f47f705efbbb
Prayash Thapa <prayasht@gmail.com>
2017-08-03 17:54:57 -0600

    Move resizing logic into a higher level component

--- src/devtools/views/right-rail/index.js
+++ src/devtools/views/right-rail/index.js
@@ -2,18 +2,12 @@ import React from 'react'
 import Presenter from 'microcosm/addons/presenter'
 import ActionFocus from './action-focus'
 import DataView from './data-view'
+import Resizable from '../resizable'
 import css from './right-rail.css'
-
-let startX
-let startWidth
 
 class RightRail extends Presenter {
   static defaultProps = {
     open: true
-  }
-
-  state = {
-    paneWidth: 0.25 * window.innerWidth
   }
 
   getModel() {
@@ -24,60 +18,27 @@ class RightRail extends Presenter {
     }
   }
 
-  initDrag = e => {
-    startX = e.clientX
-    startWidth = this.state.paneWidth
-
-    document.documentElement.addEventListener('mousemove', this.onDrag, false)
-    document.documentElement.addEventListener('mouseup', this.stopDrag, false)
-  }
-
-  onDrag = e => {
-    this.setState({
-      paneWidth: startWidth - (e.clientX - startX)
-    })
-  }
-
-  stopDrag = e => {
-    document.documentElement.removeEventListener(
-      'mousemove',
-      this.onDrag,
-      false
-    )
-    document.documentElement.removeEventListener(
-      'mouseup',
-      this.stopDrag,
-      false
-    )
-  }
-
   render() {
     const { open } = this.props
     const { snapshot, focused } = this.model
-    const { paneWidth } = this.state
+    const maxWidth = window.innerWidth * 0.4
 
     if (!open) {
       return null
     }
 
     return (
-      <aside
-        className={css.container}
-        style={{ 'min-width': paneWidth + 'px' }}
-      >
-        <div className={css.resizer} onMouseDown={this.initDrag} />
-
-        <section>
+      <Resizable defaultWidth={0.25} maxWidth={maxWidth} handlePosition="left">
+        <section className={css.pane}>
           <header className={css.header}>
             <h2 className={css.title}>State</h2>
           </header>
           <div className={css.well}>
             <DataView data={snapshot} hideRoot />
           </div>
+          <ActionFocus action={focused} />
         </section>
-
-        <ActionFocus action={focused} />
-      </aside>
+      </Resizable>
     )
   }
 }
